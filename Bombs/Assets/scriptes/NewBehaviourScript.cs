949d9ab5fd8ff179b693a5a975a292a877fd1676
Denys Babin <d.babin2173@gmail.com>
2020-03-06 16:48:22 +0100

    Start Game Dev

---
+++ Bombs/Assets/scriptes/NewBehaviourScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [ SerializeField ] Text textComp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.UpArrow)) {
+         Debug.Log(123);
+        };
+
+        if(Input.GetButton("Button")) {
+           Debug.Log(123); 
+        }
+
+
+        
+    }
+
+    // protected void OnMouseDown(Input){
+
+    // }
+}
